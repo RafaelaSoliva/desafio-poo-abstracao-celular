020dcea64e41e745e13f5a38c0c805307cff00e1
RafaelaSoliva <rafaelasoliva@outlook.com>
2025-01-02 15:15:54 -0300

    Docs: Atualização dos comentários

--- Models/Iphone.cs
+++ Models/Iphone.cs
@@ -1,10 +1,11 @@
 namespace Models;
 
 public class Iphone : Smartphone{
-
+    // Construção da classe
     public Iphone(string numero, string modelo, string imei, int memoriaTotal) : base(numero, modelo, imei, memoriaTotal) {
     }
 
+    // Implementação do método abstrato
     public override void InstalarAplicativo(string nome) {
         if (MemoriaDisponivel > 0) {
             MemoriaDisponivel --;
