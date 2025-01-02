63ebfdbc781c5ef2c1b72b0636f131fea6b7ce95
RafaelaSoliva <rafaelasoliva@outlook.com>
2025-01-02 18:43:08 -0300

    Docs: Atualização dos comentários

--- Models/Aplicativo.cs
+++ Models/Aplicativo.cs
@@ -1,9 +1,11 @@
 namespace Models;
 
 public class Aplicativo {
+    // Declaração de propriedades
     public string Nome { get; set; }
     public float Tamanho { get; set; }
 
+    // Construção da classe
     public Aplicativo(String nome, float tamanho) {
         Nome = nome;
         Tamanho = tamanho;
