538659e4f575335f0416a8c4f0328f0ae934158e
RafaelaSoliva <rafaelasoliva@outlook.com>
2025-01-03 19:19:30 -0300

    Feat: Arquivo Program - Testa os métodos da classe Nokia

--- Program.cs
+++ Program.cs
@@ -13,3 +13,19 @@ iphone.InstalarAplicativo("PUBG", 578000); // Retorna mensagem de falta de memó
 iphone.ListarAplicativosInstalados(); // Retorna lista de aplicativos instalados
 iphone.DesinstalarAplicativo("Reddit"); // Desinstala Reddit
 iphone.DesinstalarAplicativo("Reddit"); // Retorna mensagem de aplicativo não encontrado
+
+Console.WriteLine("\nTestando classe Nokia:\n");
+
+// Cria o objeto nokia
+Nokia nokia = new Nokia("88888-8888", "Nokia N95", "7492082304", 256000);
+
+// Testa todos os métodos da classe Nokia
+nokia.RealizarChamada("99999-9999"); // Realiza chamada
+nokia.ReceberChamada("99999-9999"); // Recebe chamada
+nokia.ListarAplicativosInstalados(); // Retorna mensagem de lista vazia
+nokia.InstalarAplicativo("Reddit", 28); // Instala Reddit
+nokia.InstalarAplicativo("TIDAL", 200); // Instala TIDAL
+nokia.InstalarAplicativo("PUBG", 578000); // Retorna mensagem de falta de memória
+nokia.ListarAplicativosInstalados(); // Retorna lista de aplicativos instalados
+nokia.DesinstalarAplicativo("Reddit"); // Desinstala Reddit
+nokia.DesinstalarAplicativo("Reddit"); // Retorna mensagem de aplicativo não encontrado
